3668a85b5c809b382d9ae04203f592197b36c382
Bailey Kuebelbeck <bailey.kue@gmail.com>
2022-03-19 17:32:22 -0700

    added test and function for adding a new rock

--- __tests__/rocks.test.js
+++ __tests__/rocks.test.js
@@ -12,6 +12,17 @@ describe('hand-of-resources routes', () => {
     pool.end();
   });
 
+  it('creates a new rock', async () => {
+    const newRock = {
+      name: 'basalt',
+      type: 'igneous',
+    };
+
+    const res = await request(app).post('/api/v1/rocks').send(newRock);
+
+    expect(res.body).toEqual({ id: expect.any(String), ...newRock });
+  });
+
   it('updates a rocks info by id', async () => {
     const update = { name: 'sandstone', type: 'sedimentary' };
 
